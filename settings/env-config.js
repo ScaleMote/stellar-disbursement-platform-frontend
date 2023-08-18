ab21dab23ead0b6b21c8730393494d6a9fdd80d6
Mauricio Genebrieres <mauricio@plutodao.finance>
2023-08-18 07:45:35 -0300

    Add env-config.js

---
+++ settings/env-config.js
@@ -0,0 +1,7 @@
+window._env_ = {
+    API_URL: "https://stellar-disbursement-platform-backend-dev.stellar.org",
+    STELLAR_EXPERT_URL: "https://stellar.expert/explorer/testnet",
+    HORIZON_URL: "https://horizon-testnet.stellar.org",
+    USDC_ASSET_ISSUER: "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5",
+    RECAPTCHA_SITE_KEY: "6Lego1wmAAAAAJNwh6RoOrsHuWnsciCTIL3NN-bn"
+  };
